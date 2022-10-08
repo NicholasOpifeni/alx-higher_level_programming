e8c5221f481b64278fbfb817d7c08a5d69428556
NicholasOpifeni <thanmidar@gmail.com>
2022-10-08 06:12:01 -0700

    Write a function that finds all multiple s of 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    if my_list:
-        for elm in my_list:
-            new_list.append(False if elm % 2 else True)
-            return 
+    tam = len(my_list)
+    for iter in range(0, tam):
+        if (my_list[iter] % 2) == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+            return new_list
