5d73c8b01e0df5b9f23b111ec24ea4071cad2617
NicholasOpifeni <thanmidar@gmail.com>
2022-10-25 06:47:52 -0700

    Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,6 @@
 and methods of an object"""
 
 def lookup(obj):
-    """returns list of available
-    attributes and methods of an object"""
-    return dir(obj)
+"""returns list of available
+attributes and methods of an object"""
+return dir(obj)
